a15fbf759202ead6751abe774dea6f50af1997c8
JosephNjuguna <josephnjuguna482@gmail.com>
2019-03-19 21:23:53 +0300

    update validations

--- app/middleware/validations.js
+++ app/middleware/validations.js
@@ -1,36 +1,36 @@
+const usernameValidate = (username) => {
+    const re = /[a-zA-Z]{3,}_*[0-9_]*[a-zA-Z]*_*/;
+    if(!re.test(username) || username == ''){
+        console.log("username validation failed");
+        return false
+    }else{
+        console.log('username validation passed');
+        return true
+    }
+}
 const emailValidate = (email) => {
-    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re = /(^[a-zA-Z0-9_.]+@[a-zA-Z0-9-]+\.[a-z]+$)/;
+    if (!re.test(email) || email == ''){
+        console.log('email validation failed');
+        return false
+    }else{
+        console.log('email validated');        
+        return true
+    }
+}
+const passwordValidate = (password) => {
+    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(\W|_)).{7,}$/;
 
-        if (re.test(email)){
-            console.log('email validated');        
-            return true
-        }else{
-            console.log('email validation failed');
-            return false
-        }
+    if (!re.test(password) || password == ''){
+        console.log(`${password} validation failed`);
+        return false;
+    }else{
+        console.log('password validated');
+        return true
     }
-
-    const passwordValidate = (password) => {
-        if (password.length < 5  || password == ''){
-            console.log(`${password} validation failed`);
-            return false;
-        }else{
-            console.log('password validated');
-            return true
-        }
-    }
-
-    const usernameValidate = (username) => {
-        if(username.length < 3 || username == ''){
-            console.log("username validation failed");
-            return false
-        }else{
-            console.log('username validation passed');
-            return true
-        }
-    }
-    module.exports = {
-        emailValidate,
-        passwordValidate,
-        usernameValidate
-    }
+}
+module.exports = {
+    emailValidate,
+    passwordValidate,
+    usernameValidate
+}
